c53cef5a44ec244e92dbbf55188817d973464bb7
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-16 20:09:09 +0200

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -1,60 +1,140 @@
+## Variablen
 augenzahl = 0
-while True:
-    if input.button_is_pressed(Button.A):
-        augenzahl = randint(1, 6)
-        if augenzahl == 1:
-            basic.show_leds("""
-                . . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .
-                """)
-        if augenzahl == 2:
-            basic.show_leds("""
-                . . . . .
-                . # . . .
-                . . . . .
-                . . . # .
-                . . . . .
-                """)
-        if augenzahl == 3:
-            basic.show_leds("""
-                . . . . .
-                . # . . .
-                . . # . .
-                . . . # .
-                . . . . .
-                """)
-        if augenzahl == 4:
-            basic.show_leds("""
-                . . . . .
-                . # . # .
-                . . . . .
-                . # . # .
-                . . . . .
-                """)
-        if augenzahl == 5:
-            basic.show_leds("""
-                . . . . .
-                . # . # .
-                . . # . .
-                . # . # .
-                . . . . .
-                """)
-        if augenzahl == 6:
-            basic.show_leds("""
-                . . . . .
-                . # . # .
-                . # . # .
-                . # . # .
-                . . . . .
-                """)
-        basic.pause(1000)
+spiel = "würfeln"
+## Unterprogramme für Pacman 
+def herzschlag(num: number):
+    """
+    Pacmans Herz schlägt 
+    num: Anzahl Herzschläge
+    """
+    for index in range(num):
+        basic.show_icon(IconNames.HEART)
+        basic.pause(100)
+        basic.show_icon(IconNames.SMALL_HEART)
+        basic.pause(100)
+def pacmanlinks():
+    """
+    Pacman läuft fressend nach links
+    """
+    while not (input.button_is_pressed(Button.B)):
         basic.show_leds("""
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+            # . # # .
+            . # . # #
+            . . # # #
+            . # # # #
+            # . # # .
             """)
+        basic.pause(100)
+        basic.show_leds("""
+            . . # # .
+            # # . # #
+            # # # # #
+            # # # # #
+            . . # # .
+            """)
+        basic.pause(100)
+def pacmanrechts():
+    """ 
+        Pacman läuft fressend nach rechts
+    """
+    while not (input.button_is_pressed(Button.A)):
+        basic.show_leds("""
+            . # # . #
+            # # . # .
+            # # # . .
+            # # # # .
+            . # # . #
+            """)
+        basic.pause(100)
+        basic.show_leds("""
+            . # # . .
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # . .
+            """)
+        basic.pause(100)
+    
+## Hauptprogramm
+# Endlosschleife
+while True:
+    while not (input.button_is_pressed(Button.AB)):
+        # Abfrage des gewählten Spiels
+        if spiel == "würfeln": 
+            # Abfrage des Knopfdrucks A
+            if input.button_is_pressed(Button.A):
+                # Zufallszahl zwischen 1..6 bestimmen
+                augenzahl = randint(1, 6)
+                # die Zufallszahl ist 1 => Anzeigen der Augenzahl 1
+                if augenzahl == 1:
+                    basic.show_leds("""
+                        . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+                        """)
+                # die Zufallszahl ist 2 => Anzeigen der Augenzahl 2
+                if augenzahl == 2:
+                    basic.show_leds("""
+                        . . . . .
+                        . # . . .
+                        . . . . .
+                        . . . # .
+                        . . . . .
+                        """)
+                # die Zufallszahl ist 3 => Anzeigen der Augenzahl 3
+                if augenzahl == 3:
+                    basic.show_leds("""
+                        . . . . .
+                        . # . . .
+                        . . # . .
+                        . . . # .
+                        . . . . .
+                        """)
+                # die Zufallszahl ist 4 => Anzeigen der Augenzahl 4
+                if augenzahl == 4:
+                    basic.show_leds("""
+                        . . . . .
+                        . # . # .
+                        . . . . .
+                        . # . # .
+                        . . . . .
+                        """)
+                # die Zufallszahl ist 5 => Anzeigen der Augenzahl 5
+                if augenzahl == 5:
+                    basic.show_leds("""
+                        . . . . .
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        . . . . .
+                        """)
+                # die Zufallszahl ist 6 => Anzeigen der Augenzahl 6
+                if augenzahl == 6:
+                    basic.show_leds("""
+                        . . . . .
+                        . # . # .
+                        . # . # .
+                        . # . # .
+                        . . . . .
+                        """)
+                # Kurze Pause, damit Augenzahl angezeigt bleibt
+                basic.pause(1000)
+                # Bildschirm löschen
+                basic.show_leds("""
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+        else:   # gewähltes Spiel ist Pacman
+            if input.button_is_pressed(Button.A):
+                pacmanlinks()
+            elif input.button_is_pressed(Button.B):
+                pacmanrechts()
+    if spiel == "würfeln":
+        spiel = "pacman"
+    else:
+        spiel = "würfeln"
--- main.ts
+++ main.ts
@@ -1,75 +1,164 @@
+// # Variablen
 let augenzahl = 0
-while (true) {
-    if (input.buttonIsPressed(Button.A)) {
-        augenzahl = randint(1, 6)
-        if (augenzahl == 1) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 2) {
-            basic.showLeds(`
-                . . . . .
-                . # . . .
-                . . . . .
-                . . . # .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 3) {
-            basic.showLeds(`
-                . . . . .
-                . # . . .
-                . . # . .
-                . . . # .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 4) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                . # . # .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 5) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . # . .
-                . # . # .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 6) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . # . # .
-                . # . # .
-                . . . . .
-                `)
-        }
-        
-        basic.pause(1000)
+let spiel = "würfeln"
+// # Unterprogramme für Pacman 
+function herzschlag(num: number) {
+    /** 
+    Pacmans Herz schlägt 
+    num: Anzahl Herzschläge
+    
+ */
+    for (let index = 0; index < num; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(100)
+    }
+}
+
+function pacmanlinks() {
+    /** Pacman läuft fressend nach links */
+    while (!input.buttonIsPressed(Button.B)) {
         basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+            # . # # .
+            . # . # #
+            . . # # #
+            . # # # #
+            # . # # .
             `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # # .
+            # # . # #
+            # # # # #
+            # # # # #
+            . . # # .
+            `)
+        basic.pause(100)
+    }
+}
+
+function pacmanrechts() {
+    /** Pacman läuft fressend nach rechts */
+    while (!input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . # # . #
+            # # . # .
+            # # # . .
+            # # # # .
+            . # # . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . # # . .
+            # # . # #
+            # # # # #
+            # # # # #
+            . # # . .
+            `)
+        basic.pause(100)
+    }
+}
+
+// # Hauptprogramm
+//  Endlosschleife
+while (true) {
+    while (!input.buttonIsPressed(Button.AB)) {
+        //  Abfrage des gewählten Spiels
+        if (spiel == "würfeln") {
+            //  Abfrage des Knopfdrucks A
+            if (input.buttonIsPressed(Button.A)) {
+                //  Zufallszahl zwischen 1..6 bestimmen
+                augenzahl = randint(1, 6)
+                //  die Zufallszahl ist 1 => Anzeigen der Augenzahl 1
+                if (augenzahl == 1) {
+                    basic.showLeds(`
+                        . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+                        `)
+                }
+                
+                //  die Zufallszahl ist 2 => Anzeigen der Augenzahl 2
+                if (augenzahl == 2) {
+                    basic.showLeds(`
+                        . . . . .
+                        . # . . .
+                        . . . . .
+                        . . . # .
+                        . . . . .
+                        `)
+                }
+                
+                //  die Zufallszahl ist 3 => Anzeigen der Augenzahl 3
+                if (augenzahl == 3) {
+                    basic.showLeds(`
+                        . . . . .
+                        . # . . .
+                        . . # . .
+                        . . . # .
+                        . . . . .
+                        `)
+                }
+                
+                //  die Zufallszahl ist 4 => Anzeigen der Augenzahl 4
+                if (augenzahl == 4) {
+                    basic.showLeds(`
+                        . . . . .
+                        . # . # .
+                        . . . . .
+                        . # . # .
+                        . . . . .
+                        `)
+                }
+                
+                //  die Zufallszahl ist 5 => Anzeigen der Augenzahl 5
+                if (augenzahl == 5) {
+                    basic.showLeds(`
+                        . . . . .
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        . . . . .
+                        `)
+                }
+                
+                //  die Zufallszahl ist 6 => Anzeigen der Augenzahl 6
+                if (augenzahl == 6) {
+                    basic.showLeds(`
+                        . . . . .
+                        . # . # .
+                        . # . # .
+                        . # . # .
+                        . . . . .
+                        `)
+                }
+                
+                //  Kurze Pause, damit Augenzahl angezeigt bleibt
+                basic.pause(1000)
+                //  Bildschirm löschen
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+            }
+            
+        } else if (input.buttonIsPressed(Button.A)) {
+            pacmanlinks()
+        } else if (input.buttonIsPressed(Button.B)) {
+            pacmanrechts()
+        }
+        
+    }
+    if (spiel == "würfeln") {
+        spiel = "pacman"
+    } else {
+        spiel = "würfeln"
     }
     
 }
