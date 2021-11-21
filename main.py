87f740247a113e0fa6df62cc927912b4ebe8a403
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-11-21 15:46:08 +0100

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -1,55 +1,12 @@
-augenzahl = 0
 while True:
     if input.button_is_pressed(Button.A):
-        augenzahl = randint(1, 6)
-        if augenzahl == 1:
-            basic.show_leds("""
-                . . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .
-                """)
-        if augenzahl == 2:
-            basic.show_leds("""
-                . . . . .
-                . # . . .
-                . . . . .
-                . . . # .
-                . . . . .
-                """)
-        if augenzahl == 3:
-            basic.show_leds("""
-                . . . . .
-                . # . . .
-                . . # . .
-                . . . # .
-                . . . . .
-                """)
-        if augenzahl == 4:
-            basic.show_leds("""
-                . . . . .
-                . # . # .
-                . . . . .
-                . # . # .
-                . . . . .
-                """)
-        if augenzahl == 5:
-            basic.show_leds("""
-                . . . . .
-                . # . # .
-                . . # . .
-                . # . # .
-                . . . . .
-                """)
-        if augenzahl == 6:
-            basic.show_leds("""
-                . . . . .
-                . # . # .
-                . # . # .
-                . # . # .
-                . . . . .
-                """)
+        basic.show_leds("""
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            """)
         basic.pause(1000)
         basic.show_leds("""
             . . . . .
@@ -58,3 +15,5 @@ while True:
             . . . . .
             . . . . .
             """)
+        # ODER :)
+        basic.clear_screen()
