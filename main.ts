509c6ee7fb89d9ac98343127886b465754d7fec4
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-11-23 13:40:32 +0100

    Aktualisiere main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,67 +1,52 @@
-let augenzahl = 0
 while (true) {
     if (input.buttonIsPressed(Button.A)) {
-        augenzahl = randint(1, 6)
-        if (augenzahl == 1) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 2) {
-            basic.showLeds(`
-                . . . . .
-                . # . . .
-                . . . . .
-                . . . # .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 3) {
-            basic.showLeds(`
-                . . . . .
-                . # . . .
-                . . # . .
-                . . . # .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 4) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                . # . # .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 5) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . # . .
-                . # . # .
-                . . . . .
-                `)
-        }
-        
-        if (augenzahl == 6) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . # . # .
-                . # . # .
-                . . . . .
-                `)
-        }
-        
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
         basic.pause(1000)
         basic.clearScreen()
     }
